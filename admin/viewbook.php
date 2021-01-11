56630ca07a2975c0e1912d44d2d9708665147012
Adwaith B S <adwaithbalachandran123@gmail.com>
2021-01-11 20:28:34 +0530

    lib viewbook updated

--- admin/viewbook.php
+++ admin/viewbook.php
@@ -2,5 +2,35 @@
     <?php include 'partials/nav.php'; ?>
         <?php include 'partials/sidenav.php'; ?>
 <!-- write your code hear -->
-
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Book_id</th>
+      <th scope="col">isbn</th>
+      <th scope="col">Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 <?php include 'partials/footer.php' ;?>
